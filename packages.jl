f4bc9a82d456a23df73038a975c1e9ab396ea6ba
JunYoung Kim <lgkimjy@hanyang.ac.kr>
2023-07-23 02:07:02 +0900

    updates Plots tutorial using Julia

---
+++ packages.jl
@@ -0,0 +1,14 @@
+using Pkg
+Pkg.add("DataFrames") 
+Pkg.add("CSV") 
+Pkg.add("HTTP")
+Pkg.add("Statistics")
+Pkg.add("MLDataUtils")
+Pkg.add("Impute")
+Pkg.add("Plots")
+Pkg.add("XLSX")
+
+Pkg.add("IJulia")
+
+Pkg.add("Plots")
+Pkg.add("LaTeXStrings")
